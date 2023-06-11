e0c81f129446af1bd8379e985e46396737b09225
chaima bouzid <chaima1.bouzid@gmail.com>
2023-06-12 00:00:26 +0100

    12/06/23

--- appESC/src/app/affiche-search/affiche-search.component.ts
+++ appESC/src/app/affiche-search/affiche-search.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Station } from '../Models/Station';
 import { StationService } from '../shared/services/station.service';
 import { Borne } from '../Models/Borne';
+import { Router } from '@angular/router';
+import { AuthService } from '../shared/services/auth.service';
 
 @Component({
   selector: 'app-affiche-search',
@@ -11,9 +13,20 @@ import { Borne } from '../Models/Borne';
 export class AfficheSearchComponent implements OnInit {
   stations: Station[] = [];
   borne: Borne = new Borne();
-  constructor(private stationService: StationService) {}
+  constructor(
+    private stationService: StationService,
+    private router: Router,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.stations = history.state.stations;
+  }
+  getStationDetails(id: string) {
+    this.router.navigate(['Station-details', id]);
+  }
+  logout(): void {
+    // Call the logout method from the authentication service
+    this.authService.logoutUser();
   }
 }
